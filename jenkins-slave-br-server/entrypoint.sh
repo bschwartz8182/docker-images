938ead0208b47e345d6bdf54053d2c618dfef636
bschwartz8182 <bill.schwartz@semanticbits.com>
2018-02-23 14:43:26 -0500

    Increased max connections to 2000

--- jenkins-slave-br-server/entrypoint.sh
+++ jenkins-slave-br-server/entrypoint.sh
@@ -4,5 +4,5 @@ echo "******************** SSH Demon *****************"
 /usr/sbin/sshd
 echo "*************************************************"
 echo "Starting mysql server"
-/usr/local/bin/docker-entrypoint.sh mysqld --sql_mode="STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+/usr/local/bin/docker-entrypoint.sh mysqld --max_connections=2000 --sql_mode="STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
 
